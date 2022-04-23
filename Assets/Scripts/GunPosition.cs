ab7d5e11edd33e0c1cd0ced2f48a157f09799b8a
wingyu0897 <wingyu0897@gmail.com>
2022-04-24 00:32:02 +0900

    복구
    a

--- Assets/Scripts/GunPosition.cs
+++ Assets/Scripts/GunPosition.cs
@@ -20,19 +20,11 @@ public class GunPosition : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.transform.position;
         zAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
-<<<<<<< HEAD
-        transform.position = player.transform.position + new Vector3(0.2f, 0.1f, 0);
-        transform.rotation = Quaternion.Euler(0, 0, zAngle);
-        if (zAngle > 90 || zAngle < -90)
-        {
-            transform.position = player.transform.position + new Vector3(-0.2f, 0.1f, 0);
-=======
         transform.position = player.transform.position + new Vector3(0.2f, -0.1f, 0);
         transform.rotation = Quaternion.Euler(0, 0, zAngle);
         if (zAngle > 90 || zAngle < -90)
         {
             transform.position = player.transform.position + new Vector3(-0.2f, -0.1f, 0);
->>>>>>> main
         }
     }
 }
